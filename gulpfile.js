64ee9ef673328de998dea1a2b186bd01338898f6
IvanTadeoHuerta <ivantec5sem@gmail.com>
2017-08-04 18:11:12 -0500

    Se agrego minify de archivos css con gulp, funcionado todo ok

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,23 @@
  * Dependencias
  */
 var gulp = require('gulp');
-
+var minifycss =require('gulp-minify-css');
 
 var config = {
-    sourcesDir: 'src/img/*.*',
-    bowerDir: 'dist/img'
+    sourcesDir: 'src',
+    bowerDir: 'dist'
 }
 
+// Tarea para copiar las imagenes a la carpeta dist/img
 gulp.task('imagenes', function() {
-    gulp.src([config.sourcesDir]).pipe(gulp.dest(config.bowerDir));
+    gulp.src([config.sourcesDir+'/img/*.*']).pipe(gulp.dest(config.bowerDir+'/img'));
+});
+
+
+// Tarea para minificar archivos css y transladarlos a la carpeta dist/css
+gulp.task('minify-css', function () {
+  gulp.src(config.sourcesDir+'/css/*.css')
+  //.pipe(concat('app.css'))
+  .pipe(minifycss())
+  .pipe(gulp.dest(config.bowerDir+'/css'))
 });
